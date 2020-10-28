74240132e03171f521935cd9496d7bc0ddd03337
John Chan <johnchancontact@gmail.com>
2020-10-28 15:03:07 -0400

    initial commit

---
+++ components/PageLayout.js
@@ -0,0 +1,23 @@
+import { Container } from 'react-bootstrap';
+import NavBar from './Navbar';
+
+// A wrapper 
+function PageLayout({ children, className }) {
+  return (
+    <Container>
+      <NavBar />
+      <div className={`page-wrapper ${className}`}>
+        {children}
+      </div>
+      <footer className="page-footer">
+        <div>
+          <a href="#">courses</a>{' | '}
+          <a href="#">github</a>{' | '}
+          <a href="#">facebook</a>
+        </div>
+      </footer>
+    </Container>
+  )
+}
+
+export default PageLayout
