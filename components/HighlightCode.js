ded5e6f516a2e59dc59e08145ae2ec7284896092
John Chan <johnchancontact@gmail.com>
2020-10-29 18:17:17 -0400

    code styling

---
+++ components/HighlightCode.js
@@ -0,0 +1,30 @@
+import highlight from 'highlight.js';
+import { createRef, useEffect } from 'react';
+import { findDOMNode } from 'react-dom';
+
+
+function HighlightCode({ children, language }) {
+  // createRef() receives the underlying DOM element as its current property. 
+  // When the ref attribute is used on a custom class component, 
+  // the ref object receives the mounted instance of the component as its current. 
+  // You may not use the ref attribute on function components because they don't have instances.
+  const code = createRef();
+
+  useEffect(() => {
+    debugger
+    highlight.highlightBlock(findDOMNode(code.current));
+  }, [])
+
+  return (
+    <pre>
+      <code
+        ref={code}
+        className={language}
+      >
+        {children}
+      </code>
+    </pre>
+  )
+}
+
+export default HighlightCode
--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,8 +1,8 @@
 import PageLayout from 'components/PageLayout';
 import BlogHeader from 'components/BlogHeader';
+import BlogContent from 'components/BlogContent';
 import { getAllBlogs, getBlogBySlug } from 'lib/api';
 import { Row, Col } from 'react-bootstrap';
-
 
 function BlogDetail({blog}) {
 
@@ -23,7 +23,9 @@ function BlogDetail({blog}) {
             date={blog.date}
           />
           <hr/>
-          {/* Blog Content Here */}
+          <BlogContent 
+            content={blog.content} //bc api has content appended in query. see lib/api
+          />
           It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
         </Col>
       </Row>
