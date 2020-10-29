8a812e368b056ee6a71f7b669be1fdd3e28dc462
John Chan <johnchancontact@gmail.com>
2020-10-29 10:24:00 -0400

    added author images, dates and author data

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,28 @@ import CardListItem from 'components/CardListItem';
 import { getAllBlogs } from 'lib/api';
 
 function Home({ blogs }) {
-  // debugger 
+  // debugger - hover on blog in inspect and go to sources + reload to see object
   return (
     <PageLayout>
       <AuthorIntro />
       <hr/>
-      {JSON.stringify(blogs)}
       <Row className="mb-5">
-        <Col md="10">
+       {/* <Col md="10">
           <CardListItem />
-        </Col>
-        <Col md="4">
-          <CardItem />
-        </Col>
+          </Col> */}
+        
+      { blogs.map(blog => 
+        <Col key={blog.slug} md="4">
+          <CardItem 
+            author={blog.author}
+            title={blog.title}
+            subtitle={blog.subtitle}
+            date={blog.date}
+            image={blog.coverImage}
+          />  
+        </Col> 
+        )
+      }
       </Row>
     </PageLayout>
   )
@@ -37,3 +46,33 @@ export async function getStaticProps() {
     }
   }
 }
+
+// ****************************** getServerSideProps() **********************
+// This example shows dynamically rendered pages, which are built upon request
+// executes each time at request, and builds and serves up new html/page
+// Above example with getStaticProp builds the html, js and css and then caches it
+// and serves up the same html, js, css from that build. In order for it to change
+// is for you to run yarn run build or npm run build.
+
+// export async function getServerSideProps() {
+//   const randomNumber = Math.random(); 
+//   const blogs = await getAllBlogs();
+//   return {
+//     props: {
+//       blogs,
+//       randomNumber
+//     }
+//   }
+// }
+
+//********************************************************************* */
+
+// Static Page
+// Faster, can be cached using CDN
+// Created at build time
+// When we make the request we are always receiving the same html document
+
+// Dynamic Page 
+// Created at request time (we can fetch data on server)
+// Little bit slower. The time depends on the data you are fetching. 
+
